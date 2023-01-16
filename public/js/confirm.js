286fc891a8d1a389fead005e0f8410672cf3944d
garitaku <garitaku@gmail.com>
2023-01-16 22:17:40 +0900

    削除確認機能まで実装

--- public/js/confirm.js
+++ public/js/confirm.js
@@ -1,5 +1,8 @@
-function deleteHandle(e) {
+function deleteHandle(event) {
+    //一旦formをストップ
+    event.preventDefault();
     if (window.confirm('本当に削除していいですか?')) {//削除押された時に
+        //削除OKなら再開
         document.getElementById('delete-form').submit();
     } else {
         alert('キャンセルしました');
